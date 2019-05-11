6b6af24593795411237b9b13c9db4babd803025e
minh5 <minh.v.mai@gmail.com>
2019-05-11 16:57:11 -0400

    lint

--- network.py
+++ network.py
@@ -1,7 +1,6 @@
-
 import pathlib
 
-from keras.layers import  Dense
+from keras.layers import Dense
 from keras.layers import Embedding
 from keras.layers import Input
 from keras.layers import LSTM
@@ -11,7 +10,7 @@ import numpy as np
 from preprocess import *
 
 
-DATA_PATH  = os.path.join(pathlib.Path.home(), 'minhbot/messages/inbox')
+DATA_PATH = os.path.join(pathlib.Path.home(), "minhbot/messages/inbox")
 
 # preprocessing
 inputs, outputs = create_inputs_outputs(DATA_PATH)
@@ -19,7 +18,7 @@ inputs, outputs = truncate_words(inputs, outputs, 300)
 input_corpus = create_corpus(inputs)
 output_corpus = create_corpus(outputs)
 vec_inputs = vectorize_words(input_corpus, inputs)
-vec_outputs = vectorize_words(output_corpus, outputs, 'output')
+vec_outputs = vectorize_words(output_corpus, outputs, "output")
 
 # Model Variables
 MAX_ENCODER_SEQ_LEN = max([len(txt.split()) for txt in inputs])
@@ -33,52 +32,54 @@ LATENT_DIM = 256  # Latent dimensionality of the encoding space.
 
 # initializing training data
 encoder_input_data = np.zeros(
-    (len(inputs), MAX_ENCODER_SEQ_LEN, NUM_ENCODER_TOKENS),
-    dtype='float32')
+    (len(inputs), MAX_ENCODER_SEQ_LEN, NUM_ENCODER_TOKENS), dtype="float32"
+)
 decoder_input_data = np.zeros(
-    (len(inputs), MAX_DECODER_SEQ_LEN, NUM_DECODER_TOKENS),
-    dtype='float32')
+    (len(inputs), MAX_DECODER_SEQ_LEN, NUM_DECODER_TOKENS), dtype="float32"
+)
 decoder_target_data = np.zeros(
-    (len(inputs), MAX_DECODER_SEQ_LEN, NUM_DECODER_TOKENS),
-    dtype='float32')
+    (len(inputs), MAX_DECODER_SEQ_LEN, NUM_DECODER_TOKENS), dtype="float32"
+)
 
 
 for i, (input_sentence, output_sentence) in enumerate(zip(inputs, outputs)):
     for t, word in enumerate(input_sentence.split()):
-        encoder_input_data[i, t, input_corpus[word]] = 1.
+        encoder_input_data[i, t, input_corpus[word]] = 1.0
     for t, word in enumerate(output_sentence.split()):
         # decoder_target_data is ahead of decoder_input_data by one timestep
-        decoder_input_data[i, t, output_corpus[word]] = 1.
+        decoder_input_data[i, t, output_corpus[word]] = 1.0
         if t > 0:
             # decoder_target_data will be ahead by one timestep
             # and will not include the start character.
-            decoder_target_data[i, t - 1, output_corpus[word]] = 1.
+            decoder_target_data[i, t - 1, output_corpus[word]] = 1.0
 
 
 # Define an input sequence and process it.
 encoder_inputs = Input(shape=(None,))
 x = Embedding(NUM_ENCODER_TOKENS, LATENT_DIM)(encoder_inputs)
-x, state_h, state_c = LSTM(LATENT_DIM,
-                           return_state=True)(x)
+x, state_h, state_c = LSTM(LATENT_DIM, return_state=True)(x)
 encoder_states = [state_h, state_c]
 
 # Set up the decoder, using `encoder_states` as initial state.
 decoder_inputs = Input(shape=(None,))
 x = Embedding(NUM_DECODER_TOKENS, LATENT_DIM)(decoder_inputs)
 x = LSTM(LATENT_DIM, return_sequences=True)(x, initial_state=encoder_states)
-decoder_outputs = Dense(NUM_DECODER_TOKENS, activation='softmax')(x)
+decoder_outputs = Dense(NUM_DECODER_TOKENS, activation="softmax")(x)
 
 # Define the model that will turn
 # `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
 model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
 
 # Compile & run training
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+model.compile(optimizer="rmsprop", loss="categorical_crossentropy")
 # Note that `decoder_target_data` needs to be one-hot encoded,
 # rather than sequences of integers like `decoder_input_data`!
-model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
-          batch_size=BATCH_SIZE,
-          epochs=EPOCHS,
-          validation_split=0.2)
+model.fit(
+    [encoder_input_data, decoder_input_data],
+    decoder_target_data,
+    batch_size=BATCH_SIZE,
+    epochs=EPOCHS,
+    validation_split=0.2,
+)
 # Save model
-model.save('s2s.h5')
+model.save("s2s.h5")
--- preprocess.py
+++ preprocess.py
@@ -4,18 +4,18 @@ import os
 import re
 
 
-MY_NAME = 'Minh Mai'
+MY_NAME = "Minh Mai"
 
 
 def validate_data(data):
     """Validate data before parsing message"""
     # ensures two participant in conversation,
     # no group chats or one way conversations
-    if len(set([user['name'] for user in data['participants']])) != 2:
+    if len(set([user["name"] for user in data["participants"]])) != 2:
         return False
-            
+
     # ensure some level of length and back and forth in conversations
-    if len(data['messages']) < 4:
+    if len(data["messages"]) < 4:
         return False
 
     return True
@@ -23,8 +23,8 @@ def validate_data(data):
 
 def normalize_content(message):
     """Normalize strings by removing special characters"""
-    content = message.get('content', '')
-    msg = re.sub(r'[^ a-zA-Z0-9]', '', content)
+    content = message.get("content", "")
+    msg = re.sub(r"[^ a-zA-Z0-9]", "", content)
     return msg.lower()
 
 
@@ -35,10 +35,10 @@ def process_messages(data, other_person):
 
     for msg in data:
 
-        if msg['sender_name'] == current_person:
+        if msg["sender_name"] == current_person:
             current_texts.append(normalize_content(msg))
         else:
-            combined_text = '.'.join(current_texts)
+            combined_text = ".".join(current_texts)
 
             if current_person == MY_NAME:
                 outputs.append(combined_text)
@@ -48,10 +48,10 @@ def process_messages(data, other_person):
                 current_person = MY_NAME
 
             current_texts = [normalize_content(msg)]
-    
+
     # sometimes I don't respond to people. Whoops
     if len(inputs) != len(outputs):
-        inputs = inputs[:len(outputs)]
+        inputs = inputs[: len(outputs)]
 
     return inputs, outputs
 
@@ -62,22 +62,22 @@ def create_inputs_outputs(root_dir):
     all_outputs = []
 
     for filez in os.listdir(root_dir):
-        message_path = os.path.join(root_dir, filez, 'message.json')
+        message_path = os.path.join(root_dir, filez, "message.json")
 
         if not os.path.exists(message_path):
             continue
 
-        with open(message_path, 'r') as fd:
+        with open(message_path, "r") as fd:
             data = json.load(fd)
-            
+
             if not validate_data(data):
                 continue
-        
-        other_person = [user['name']
-            for user in data['participants']
-            if user['name'] != MY_NAME][0]
-        messages = sorted(data['messages'], key=lambda k: k['timestamp_ms'])
-        start_number = [msg['sender_name'] for msg in messages].index(other_person)
+
+        other_person = [
+            user["name"] for user in data["participants"] if user["name"] != MY_NAME
+        ][0]
+        messages = sorted(data["messages"], key=lambda k: k["timestamp_ms"])
+        start_number = [msg["sender_name"] for msg in messages].index(other_person)
         inputs, outputs = process_messages(messages[start_number:], other_person)
         all_inputs.extend(inputs)
         all_outputs.extend(outputs)
@@ -97,20 +97,20 @@ def create_corpus(data):
                 results[word] = current_number
                 current_number += 1
 
-    results['\t'] = current_number + 1
-    results['\n'] = current_number + 2
+    results["\t"] = current_number + 1
+    results["\n"] = current_number + 2
     return results
 
 
-def vectorize_words(corpus, list_of_sentences, vector_type='input'):
+def vectorize_words(corpus, list_of_sentences, vector_type="input"):
     """Convert words to their numeric equivalent"""
     results = []
     for sentence in list_of_sentences:
         indexed = [corpus[word] for word in sentence.split()]
 
         # adding start and stopping indicators for sequence
-        if vector_type == 'output':
-            indexed = [corpus['\t']] + indexed + [corpus['\n']]
+        if vector_type == "output":
+            indexed = [corpus["\t"]] + indexed + [corpus["\n"]]
 
         results.append(indexed)
     return results
